5f2288d36219112461b0103e7fc6d568ca7ba8c6
cyberich1009 <50173540+cyberich1009@users.noreply.github.com>
2019-05-03 15:32:23 +0800

    Update HelloWorld.java

--- sample/pipeline-as-code/src/main/java/com/systex/HelloWorld.java
+++ sample/pipeline-as-code/src/main/java/com/systex/HelloWorld.java
@@ -2,6 +2,7 @@ package com.systex;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World! Hello World!");
+        System.out.println("Hello World!\n Jenkins!");
+        System.out.println(new java.util.Date());
     }
 }
